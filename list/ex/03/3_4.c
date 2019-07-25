e20103ff7171ef0018c3318585e7856eb243da21
shibata <masataka.shibata.ss@gmail.com>
2019-07-25 15:50:20 +0900

    0725

--- list/ex/03/3_4.c
+++ list/ex/03/3_4.c
@@ -2,14 +2,6 @@
 
 int main(void)
 {
-	double num1;
-	double num2;
-	printf("身長を入力してください\n" );
-	scanf ("%lf", &num1);
-	printf("体重を入力してください\n" );
-	scanf ("%lf", &num2);
-	printf("身長は%fセンチです。\n", num1);
-	printf("体重は%fキロです。\n", num2);
- 
+
    return 0;
 }
--- list/ex/03/3_5.c
+++ list/ex/03/3_5.c
@@ -2,12 +2,6 @@
 
 int main(void)
 {
-	double num1;
-	double num2;
-	printf("身長と体重を入力してください\n" );
-	scanf ("%lf", &num1);
-	scanf ("%lf", &num2);
-	printf("身長は%fセンチ：体重は%fです。\n", num1, num2);
- 
+
    return 0;
 }
