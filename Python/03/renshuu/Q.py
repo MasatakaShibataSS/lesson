fd11ab358f0e6a2a2fec02dd5322398e46a0fd32
shibata <masataka.shibata.ss@gmail.com>
2019-08-26 15:34:41 +0900

    0826pm

---
+++ Python/03/renshuu/Q.py
@@ -0,0 +1,6 @@
+tall = float(input("身長を入力してください。"))
+bmi = 22
+print("身長: ", tall,"cm")
+
+hyoujun = bmi * (tall/100)**2
+print("標準体重: ", hyoujun, "kg")
---
+++ Python/04/ASample.py
@@ -0,0 +1,10 @@
+# res = input("売上は好調ですか？(○×)")
+# ans = "好調" if (res=="○") else "普通"
+# print(ans)
+
+res = input("売上は好調ですか？(○×)")
+if (res=="○"):
+    ans = "好調"
+else:
+    ans = "普通"
+print (ans)
