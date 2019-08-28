465459b4240e82aeb249ea0b1d3c76f72fccb22c
shibata <masataka.shibata.ss@gmail.com>
2019-08-28 10:04:54 +0900

    082801

--- Python/04/rehshuu/1-1.py
+++ Python/04/rehshuu/1-1.py
@@ -1,18 +1,20 @@
 month = int(input("月の数値(1～12)を入力してください: "))
 
-if(month == 12) or (month == 1) or (month == 2):
+# if(month == 12) or (month == 1) or (month == 2):
+if month in [12, 1, 2]:
 
     print("冬")
 
-elif (month >= 3) and (month <= 5):
+elif 3 <= month <= 5:
 
     print("春")
 
-elif (month >= 6) and (month <= 8 ):
+elif 6 <= month <= 8:
 
     print("夏")
 
-elif (month <= 9 ) and (month < 11):
+elif 9 <= month <= 11:
+# elif (month <= 9 ) and (month < 11):
 
     print("秋")
 
