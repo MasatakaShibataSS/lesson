e654a01cac56ca11e2b2beea56328237904f2520
shibata <masataka.shibata.ss@gmail.com>
2019-11-11 09:46:04 +0900

    1108

---
+++ AI/examples/06/classify_handsign_sa.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python classify_handsign_1.py 4 ./data/m01 ./data/m05 ./data/m06 ./data/m07 ./data/m02 ./data/m03 ./data/m04 ./data/m08 ./data/m09 ./data/m10 ./data/m11 ./data/m12 ./data/m13 ./data/m14 ./data/m15 ./data/m16
